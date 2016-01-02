1e2f17305c11b045363380b016a652e4d94b8ab6
odulzaides <dulzaides.oscar@gmail.com>
2016-01-01 23:55:25 -0800

    "App finished and saving task as well as reloading them when reloading page."

--- taskathand.js
+++ taskathand.js
@@ -1,94 +1,124 @@
-'use strict';
+"use strict";
 
 function TaskAtHandApp()
 {
-    var version = "v1.0";
+    var version = "v1.3",
+        appStorage = new AppStorage("taskAtHand");
 
     function setStatus(message)
     {
         $("#app>footer").text(message);
     }
 
+    function saveTaskList()
+    {
+        var tasks = [];
+        $("#task-list .task span.task-name").each(function() {
+            tasks.push($(this).text())
+        });
+        appStorage.setValue("taskList", tasks);
+    }
+    
     function addTask()
     {
         var taskName = $("#new-task-name").val();
-        if (taskName) {
+        if (taskName)
+        {
             addTaskElement(taskName);
-            //Reset the text field.
+            // Reset the field
             $("#new-task-name").val("").focus();
+            saveTaskList();
         }
     }
-
+    
     function addTaskElement(taskName)
     {
-        var $task = $('#task-template .task').clone();
+        var $task = $("#task-template .task").clone();
         $("span.task-name", $task).text(taskName);
 
         $("#task-list").append($task);
+        
+        // Button events
+        $("button.delete", $task).click(function() { removeTask($task); });
+        $("button.move-up", $task).click(function() { moveTask($task, true); });
+        $("button.move-down", $task).click(function() { moveTask($task, false); });
 
-        $("button.delete", $task).click(function() {
-            $task.remove();
-        });
-        $("button.move-up", $task).click(function()
+        // Task name events
+        $("span.task-name", $task).click(function() { onEditTaskName($(this)); });
+        $("input.task-name", $task).change(function() { onChangeTaskName($(this)); })
+                              .blur(function() { $(this).hide().siblings("span.task-name").show(); });
+    }
+    
+    function removeTask($task)
+    {
+        $task.remove();
+        saveTaskList();
+    }
+    
+    function moveTask($task, moveUp)
+    {
+        if (moveUp)
         {
             $task.insertBefore($task.prev());
-        });
-        $("button.move-down", $task).click(function()
+        }
+        else
         {
             $task.insertAfter($task.next());
-        });
-        $("span.task-name", $task).click(function() {
-            onEditTaskName($(this));
-        });
-        $("input.task-name", $task).change( function  ()
-            {
-                onChangeTaskName($(this));
-            });
-
-
-        function onEditTaskName($span)
-        {
-            $span.hide()
-                .siblings("input.task-name")
-                .val($span.text())
-                .show()
-                .focus();
         }
-            function onChangeTaskName ($input)
-            {
-                $input.hide();
-                var $span = $input.siblings("span.task-name");
-                if ($input.val())
-                {
-                  $span.text($input.val());
-                }
-                $span.show();
-            }
-
-
+        saveTaskList();
     }
 
-    this.start = function() {
-        $("#new-task-name").keypress(function(e)
-            {
-                if (e.which == 13) // Enter key
-                {
-                    addTask();
-                    return false;
-                }
-            })
-            .focus();
+    function onEditTaskName($span)
+    {
+        $span.hide()
+             .siblings("input.task-name").val($span.text()).show().focus();
+    }
 
+    function onChangeTaskName($input)
+    {
+        $input.hide();
+        var $span = $input.siblings("span.task-name");
+        if ($input.val())
+        {
+            $span.text($input.val());
+            saveTaskList();
+        }
+        $span.show();
+    }
+    
+    function loadTaskList()
+    {
+        var tasks = appStorage.getValue("taskList");
+        if (tasks)
+        {
+            for (var i in tasks)
+            {
+                addTaskElement(tasks[i]);
+            }
+        }
+    }
+    
+    this.start = function()
+    {
+        $("#new-task-name").keypress(function(e)
+        {
+            if (e.which == 13) // Enter key
+            {
+                addTask();
+                return false;
+            }
+        })
+        .focus();
+        
         $("#app>header").append(version);
+        
+        loadTaskList();
         setStatus("ready");
     };
 }
 
-
-
 $(function()
 {
-
     window.app = new TaskAtHandApp();
     window.app.start();
 });
